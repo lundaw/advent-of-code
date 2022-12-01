6b8f005a0abf6674e86e740f99f6b7055da12e63
lundaw <38364541+lundaw@users.noreply.github.com>
2022-12-01 11:11:33 +0100

    Add Python 2022 Day 1 solution

--- 2022/Python/aoc/day1/day1.py
+++ 2022/Python/aoc/day1/day1.py
@@ -1,3 +1,4 @@
+import functools
 import os.path
 
 from common.base_day import BaseDay
@@ -7,10 +8,15 @@ class Day1(BaseDay):
     def _parse_input(self) -> None:
         input_path = os.path.join(self.input_root_folder, "day1")
         with open(file=input_path, mode="r", encoding="utf-8") as input_file:
-            self.input = input_file.read().splitlines()
+            self.input = [functools.reduce(
+                lambda accumulator, current: accumulator + int(current), line.splitlines(), 0
+            ) for line in input_file.read().split("\n\n")]
 
     def run_part1(self) -> int:
-        pass
+        return max(self.input)
 
     def run_part2(self) -> int:
-        pass
+        sorted_ = self.input.copy()
+        sorted_.sort(reverse=True)
+
+        return sum(sorted_[:3])
